8cbba78e51680fd24e04f4c35e78469b1c4089d7
Blazin64 <26800120+Blazin64@users.noreply.github.com>
2019-04-27 22:43:44 -0500

    Remove header from movies.csv

--- predict.py
+++ predict.py
@@ -4,11 +4,15 @@ from surprise import Dataset, evaluate, Reader, SVDpp, SVD
 
 
 # Predict top movies for a user
-def predict_top_movies(predictions, k=5):
+def predict_top_movies(my_users, predictions, k=5):
     top_movies = defaultdict(list)
     for user, item, truth, prediction, _ in predictions:
-        top_movies[user].append((item, prediction))
-    return None
+        if user in my_users:
+            top_movies[user].append((item, prediction))
+    for user, ratings in top_movies.items():
+        ratings.sort(key=lambda x: x[1], reverse=True)
+        top_movies[user] = ratings[:k]
+    return top_movies
 
 
 # Define the format
@@ -16,14 +20,19 @@ reader = Reader(line_format='user item rating timestamp', sep=',')
 # Load the data from the file using the reader format
 data = Dataset.load_from_file('./ml-latest-small/ratings.csv', reader=reader)
 # Split MovieLens data into 3 folds
-data.split(n_folds=3)
+data.split(n_folds=2)
 # Use the SVD algorithm for prediction
 method = SVD()
 # Evaluate the results with RMSE and MAE
 evaluate(method, data, measures=['RMSE', 'MAE'])
 # Train with the dataset
-method.fit(data.build_full_trainset())
-# Predict the rating this user will rate this movie
-user = 196
-item = 303
-print(method.predict(str(user), str(item)))
+trainset = data.build_full_trainset()
+method.fit(trainset)
+# Test on an anti testset
+predictions = method.test(trainset.build_anti_testset())
+# User range
+users = ["196", "197"]
+# Predict the top movies (top 5 by default) for a set of users
+top_movies = predict_top_movies(users, predictions)
+for user_id, ratings in top_movies.items():
+    print(user_id, [iid for (iid, _) in ratings])
