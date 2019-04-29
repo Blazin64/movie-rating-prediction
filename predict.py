208a30a4915c66289af5c8aa9ba282ac4212be68
Blazin64 <26800120+Blazin64@users.noreply.github.com>
2019-04-29 11:42:44 -0500

    RMSE & MAE display, tweaks

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 from tkinter import Tk, Button, Label, Entry, scrolledtext, DISABLED, END, NORMAL
 import csv
+import numpy
 from collections import defaultdict
 from surprise import Dataset, Reader, SVD
 from surprise.model_selection import cross_validate
@@ -11,9 +12,7 @@ import psutil
 
 class MovieRatings:
     def __init__(self, master):
-        # Label(master, text='User selection').grid(row=0)
-        # e1 = Entry(master)
-        # e1.grid(row=0, column=1)
+        # Set up the GUI
         self.master = master
         self.text = scrolledtext.ScrolledText(master, width=100, height=30)
         self.text.grid(row=6, column=0,)
@@ -23,7 +22,7 @@ class MovieRatings:
         self.button.grid(row=1, column=0)
         self.msg3 = Label(master, text="2. Enter user IDs you would like to display predictions for (1-610).")
         self.msg3.grid(row=2, column=0)
-        self.msg4 = Label(master, text="Use CSV format with no spaces.")
+        self.msg4 = Label(master, text="Separate values with commas. No spaces.")
         self.msg4.grid(row=3, column=0)
         self.userentry = Entry(master, width=40)
         self.userentry.grid(row=4, column=0)
@@ -32,34 +31,41 @@ class MovieRatings:
         self.button2.configure(state=DISABLED)
         master.mainloop()
 
+    # Used to print to the UI text box
     def printer(self, string):
         self.text.insert(END, string + "\n")
         self.master.update()
 
+    # Enable a button
     def enableButton(self, button):
         button.configure(state=NORMAL)
         self.master.update()
 
+    # Dsable a button
     def disableButton(self, button):
         button.configure(state=DISABLED)
         self.master.update()
 
+    # Change the text on a button.
     def updateButton(self, string, button):
         button["text"] = string
         self.master.update()
 
+    # Train on the dataset and predict ratings
     def runner(self):
         self.updateButton('Training now...', self.button)
         self.disableButton(self.button)
         results, trainset = self.trainer()
         self.updateButton('Training done!', self.button)
-        self.enableButton(self.button2)
         self.predictions = self.predictor(results, trainset)
+        self.enableButton(self.button2)
 
+    # Display top 10 predictions for selected users
     def runner2(self):
         users = self.userentry.get().split(",")
         self.finder(self.predictions, users)
 
+    # Get top 10 predictions
     def finder(self, predictions, users):
         top_movies = self.get_top_movies(users, predictions, 10)
         # Collect the movie IDs for the users' top movies
@@ -83,10 +89,12 @@ class MovieRatings:
                 self.printer("\t\tPredicted rating: {:0.3f}".format(prediction) + "\n")
                 counter += 1
 
+    # Predict ratings.
     def predictor(self, results, trainset):
         self.printer("\nNow predicting ratings for movies each user hasn't rated.")
         self.printer("Please wait...\n")
-        # Test on an anti testset
+        # Test on an anti testset (effectively predicts how users will rate
+        # movies they haven't watched yet).
         start = time.time()
         predictions = results.test(trainset.build_anti_testset())
         end = time.time()
@@ -98,9 +106,10 @@ class MovieRatings:
         self.printer("{:0.5f}".format(process.memory_info().vms/1048576.0) + " MB Virtual")
         return predictions
 
+    # Train with the dataset
     def trainer(self):
         # Reassurance that the script is actually running.
-        self.printer("\nNow training on the MovieLens latest small dataset.")
+        self.printer("\nNow training on the MovieLens latest small dataset. (8 folds used)")
         self.printer("Please wait...\n")
         # Define the file's format
         reader = Reader(line_format='user item rating timestamp', sep=',')
@@ -112,7 +121,19 @@ class MovieRatings:
         # Use 8-fold cross validation and evaluate the results with RMSE and MAE
         measurements = cross_validate(method, data, measures=['RMSE', 'MAE'],
                                       cv=8, verbose=False, n_jobs=-2, return_train_measures=True)
-        print(measurements)
+        # Show the stats
+        meanFitTime = numpy.mean(measurements["fit_time"])
+        meanTestTime = numpy.mean(measurements["test_time"])
+        meanTestMAE = numpy.mean(measurements["test_mae"])
+        meanTestRMSE = numpy.mean(measurements["test_rmse"])
+        meanTrainMAE = numpy.mean(measurements["train_mae"])
+        meanTrainRMSE = numpy.mean(measurements["train_rmse"])
+        self.printer("Mean fit time per fold: {:0.5f} seconds".format(meanFitTime))
+        self.printer("Mean test time per fold: {:0.5f} seconds".format(meanTestTime))
+        self.printer("Mean train MAE per fold: {:0.5f}".format(meanTrainMAE))
+        self.printer("Mean train RMSE per fold: {:0.5f}".format(meanTrainRMSE))
+        self.printer("Mean test MAE per fold: {:0.5f}".format(meanTestMAE))
+        self.printer("Mean test RMSE per fold: {:0.5f}\n".format(meanTestRMSE))
         # Train with the dataset
         trainset = data.build_full_trainset()
         method.fit(trainset)
